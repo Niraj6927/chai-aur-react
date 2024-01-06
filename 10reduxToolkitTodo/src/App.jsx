c798bbbe4e27eb853f89ccfd4328fa7b378044d7
Niraj Kumar Sharma <nirajkumarsvm@gmail.com>
2024-01-06 21:13:34 +0530

    reduxtoolkitTodo project added

---
+++ 10reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Todos from './components/Todos';
+import AddTodo from './components/addTodo';
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn About redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App
